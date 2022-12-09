36deff0a5eeb355f9be78e4bc03ba44dfc321ce3
Nathan Joudet <nathanjoudet@orange.fr>
2022-12-09 13:22:25 +0100

    feat: ajout des interfaces :sparkles:

--- Interfaces/Guerrier.ts
+++ Interfaces/Guerrier.ts
@@ -3,7 +3,7 @@ export default interface Guerrier {
 
     /**
      * Lance une attaque physique qui fait des dégâts !
-     * La puissance de l'attaque est proportionnelle à l'énergie utilisée.
+     * La puissance de l'attaque est proportionnelle à l'énergie utilisée
      * @param nombreDenergie Nombre d'énergie à utiliser pour l'attaque
      */
     attaquePuissante(nombreEnergie: number): number;
--- Interfaces/Magique.ts
+++ Interfaces/Magique.ts
@@ -3,7 +3,7 @@ export default interface Magique {
 
     /**
      * Lance une attaque magique qui fait des dégâts !
-     * La puissance du spell est propotionnelle aux PM utilisés.
+     * La puissance du spell est propotionnelle aux PM utilisés
      * @param nombreDePm Nombre de PM à utiliser pour l'attaque
      */
     attaqueMagique(nombreDePm: number): number;
